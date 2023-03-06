f24d090b1d3bd0b5925f8f64277c27ef48bb333d
mrchrisoliver <mrchrisoliver@users.noreply.github.com>
2023-03-06 11:10:57 +0000

    🪄 Code Style Fixes

--- src/PokemonClient.php
+++ src/PokemonClient.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace mrchrisoliver\Package;
 
-use mrchrisoliver\Package\Resources\PokemonResource;
 use Saloon\Http\Connector;
+use mrchrisoliver\Package\Resources\PokemonResource;
 
 final class PokemonClient extends Connector
 {
--- tests/Feature/Pokemon/GetAllPokemonTest.php
+++ tests/Feature/Pokemon/GetAllPokemonTest.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use mrchrisoliver\Package\PokemonClient;
 
-it('can send a request to get all pokemon', function() {
+it('can send a request to get all pokemon', function () {
     $client = new PokemonClient();
 
     $allPokemon = $client->pokemon()->all()->object()->results;
